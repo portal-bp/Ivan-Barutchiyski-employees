7e2064aee10f68271974bed01a0055503a9a487b
portal-bp <ivan.barutchiyski@gmail.com>
2025-09-10 15:29:07 +0300

    Init spring

---
+++ ibempapp/src/main/java/com/ib/ibempapp/IbempappApplication.java
@@ -0,0 +1,13 @@
+package com.ib.ibempapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbempappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbempappApplication.class, args);
+	}
+
+}
---
+++ ibempapp/src/test/java/com/ib/ibempapp/IbempappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib.ibempapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbempappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
